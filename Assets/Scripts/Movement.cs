35e0eb41c83ecac6c8e6bb013337ce0dc6abb871
Jarmo Syvälahti <44726773+jarm111@users.noreply.github.com>
2017-11-20 14:49:09 +0200

    Refactored movement.

--- Assets/Scripts/Movement.cs
+++ Assets/Scripts/Movement.cs
@@ -2,32 +2,29 @@
 
 public class Movement : MonoBehaviour {
 
-    Rigidbody2D rb2d;
+    [SerializeField]
+    private float radius = 1f;
+    [SerializeField]
+    private float speed = 3;
 
     private float timer;
     private float angle;
-    private float rad = 0.5f;
-    private float centerx = 0;
-    private float centery = 0;
-    private float centerz = 0;
+    private float centerX;
+    private float centerY;
+    private Rigidbody2D rb2d;
 
     private void Start()
     {
         rb2d = GetComponent<Rigidbody2D>();
-    }
-
-    private void Update()
-    {
-        
+        centerX = transform.position.x;
+        centerY = transform.position.x;
     }
 
     private void FixedUpdate()
     {
-        //rb2d.AddForce(new Vector3(3, 0, 0));
         timer += Time.deltaTime;
-        angle = timer;
-        //this.transform.position = new Vector3((centerx + Mathf.Sin(angle) * rad), (centery + Mathf.Cos(angle) * rad), 0);
-        //rb2d.AddForce(new Vector3((centerx + Mathf.Sin(angle) * rad), (centery + Mathf.Cos(angle) * rad), 0));
-        rb2d.velocity = new Vector3((centerx + Mathf.Sin(angle) * rad), (centery + Mathf.Cos(angle) * rad), 0);
+        angle = timer * speed;
+        rb2d.velocity = new Vector2((centerX + Mathf.Sin(angle) * radius), 
+            (centerY + Mathf.Cos(angle) * radius));
     }
 }
