5dd72ce3fcb824e08b2c66c581cf3d91f65a8dba
Jarmo Syvälahti <44726773+jarm111@users.noreply.github.com>
2017-11-20 15:19:10 +0200

    Added StrafePatrolMovement script.

---
+++ Assets/Scripts/StrafePatrolMovement.cs
@@ -0,0 +1,36 @@
+﻿using UnityEngine;
+
+public class StrafePatrolMovement : MonoBehaviour {
+
+    [SerializeField]
+    private float speed = 1;
+    [SerializeField]
+    private float minY = 0;
+    [SerializeField]
+    private float maxY = 10;
+
+    private Rigidbody2D rb2d;
+
+
+    private void Start()
+    {
+        rb2d = GetComponent<Rigidbody2D>();
+    }
+
+    private void Update()
+    {
+        if (transform.position.y > maxY && speed > 0)
+        {
+            speed = -speed;
+        }
+        else if (transform.position.y < minY && speed < 0)
+        {
+            speed = -speed;
+        }
+    }
+
+    private void FixedUpdate()
+    {
+        rb2d.velocity = new Vector2(0, speed);
+    }
+}
