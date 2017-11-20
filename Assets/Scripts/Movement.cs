95888bacf975c7db88686f2a959a2ee11f98e3b3
Jarmo Syvälahti <44726773+jarm111@users.noreply.github.com>
2017-11-20 14:28:25 +0200

    Initial commit

---
+++ Assets/Scripts/Movement.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+
+public class Movement : MonoBehaviour {
+
+    Rigidbody2D rb2d;
+
+    private float timer;
+    private float angle;
+    private float rad = 0.5f;
+    private float centerx = 0;
+    private float centery = 0;
+    private float centerz = 0;
+
+    private void Start()
+    {
+        rb2d = GetComponent<Rigidbody2D>();
+    }
+
+    private void Update()
+    {
+        
+    }
+
+    private void FixedUpdate()
+    {
+        //rb2d.AddForce(new Vector3(3, 0, 0));
+        timer += Time.deltaTime;
+        angle = timer;
+        //this.transform.position = new Vector3((centerx + Mathf.Sin(angle) * rad), (centery + Mathf.Cos(angle) * rad), 0);
+        //rb2d.AddForce(new Vector3((centerx + Mathf.Sin(angle) * rad), (centery + Mathf.Cos(angle) * rad), 0));
+        rb2d.velocity = new Vector3((centerx + Mathf.Sin(angle) * rad), (centery + Mathf.Cos(angle) * rad), 0);
+    }
+}
---
+++ README.md
@@ -0,0 +1,28 @@
+# ECS model experiment
+An experiment project playing with Unity's Entity Component System model.
+
+## Features
+- Human and Alien team
+- 1 commander, 4 soldiers and 1 medic in each team
+- Commander and soldiers walk in circle and shoot random enemy team member
+- Commander moves slower and is bigger but has a shield that blocks damage
+- Medic strafes sideways behind other team and heals closest team member
+- Hud-text for remaining team members
+- Win text for each team
+
+## TODO
+
+- Movement pattern for medic
+- Weapon & hit mechanic
+- Health & death mechanic
+- Shield mechanic
+- Hud view for remaining team members
+- Hud view for game over
+
+## DONE
+- Movement pattern for soldier
+
+## Algorithms
+
+### Circle movement
+
