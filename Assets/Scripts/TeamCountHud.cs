1a1f40bf5de4898be95e4f65797417c15215ffc9
Jarmo Syvälahti <44726773+jarm111@users.noreply.github.com>
2017-11-28 14:45:41 +0200

    Implemented Canvas based UI for team count.

--- Assets/Scripts/TeamCountHud.cs
+++ Assets/Scripts/TeamCountHud.cs
@@ -1,19 +1,32 @@
 ﻿using UnityEngine;
+using UnityEngine.UI;
 
 public class TeamCountHud : MonoBehaviour {
 
-    private GUIStyle font;
+    //private GUIStyle font;
+    private Text humansCount;
+    private Text aliensCount;
 
     private void Start()
     {
-        font = new GUIStyle();
-        font.fontSize = 20;
-        font.normal.textColor = Color.yellow;
+        //font = new GUIStyle();
+        //font.fontSize = 20;
+        //font.normal.textColor = Color.yellow;
+        humansCount = GameObject.Find("HumansCountText").GetComponent<Text>();
+        aliensCount = GameObject.Find("AliensCountText").GetComponent<Text>();
+        humansCount.text = "Humans remaining: " + TeamCount.Instance.HumanCount;
+        aliensCount.text = "Aliens remaining: " + TeamCount.Instance.AlienCount;
+    }
+
+    private void Update()
+    {
+        humansCount.text = "Humans remaining: " + TeamCount.Instance.HumanCount;
+        aliensCount.text = "Aliens remaining: " + TeamCount.Instance.AlienCount;
     }
 
     private void OnGUI()
     {
-        GUI.TextField(new Rect(25, 25, 400, 100), "Humans remaining: " + TeamCount.Instance.HumanCount, font);
-        GUI.TextField(new Rect(300, 25, 400, 100), "Aliens remaining: " + TeamCount.Instance.AlienCount, font);
+        //GUI.TextField(new Rect(25, 25, 400, 100), "Humans remaining: " + TeamCount.Instance.HumanCount, font);
+        //GUI.TextField(new Rect(300, 25, 400, 100), "Aliens remaining: " + TeamCount.Instance.AlienCount, font);
     }
 }
